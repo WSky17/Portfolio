c131a212ae7e146689c79a862bfec6c492f90a27
Wsky17 <38331183+WSky17@users.noreply.github.com>
2022-06-02 18:37:39 -0700

    add exercise 3.4 Pizza Order

--- Python/100_Days_of_coding_Python_practice3.py
+++ Python/100_Days_of_coding_Python_practice3.py
@@ -50,21 +50,69 @@ Created on Tue May 17 07:56:27 2022
 
 # Exercise 3.3 Leap Year Exercise 
 
+# =============================================================================
+# # 🚨 Don't change the code below 👇
+# year = int(input("Which year do you want to check? "))
+# # 🚨 Don't change the code above 👆
+# 
+# #Write your code below this line 👇
+# 
+# if year % 4 ==0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year.")
+#         else:
+#             print ("Not leap year.")  
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not leap year.")
+# =============================================================================
+
+#Exercise 3.4 Pizza Order 
+
 # 🚨 Don't change the code below 👇
-year = int(input("Which year do you want to check? "))
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L: ")
+add_pepperoni = input("Do you want pepperoni? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+bill = 0
 
-if year % 4 ==0:
-    if year % 100 >0:
-        print ("no leap")
-    if year % 400 == 0:
-        print("A Leap year")  
-    else:
-        print("Not leeper")
+if size == "S":
+    bill += 15
+
+elif size == "M":
+    bill += 20
+
 else:
-    print("Not a Leap Year")
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill +=1
+
+print (f"Your final bill is ${bill}")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
